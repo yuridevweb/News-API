3915b5e7a362cc5a54a300d7374ff798968154e7
yuridevweb <yuridevweb@gmail.com>
2022-03-30 16:13:17 +0100

    remove console.log

--- models/news.models.js
+++ models/news.models.js
@@ -49,7 +49,6 @@ exports.selectComments = (article_id) => {
   const text = `SELECT * FROM comments WHERE article_id = $1;`
   const values = [article_id]
   return db.query(text, values).then((result) => {
-    console.log('=============')
     return result.rows
   })
 }
